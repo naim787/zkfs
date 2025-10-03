c1854b5387374ed9f1e7f2dcba7c3a7c9b2a1a20
naim787 <naimmmmab@gmail.com>
2025-10-03 05:12:35 -0400

    Update: 10/3/2025, 5:12:35 AM

--- src/lib/crud.js
+++ src/lib/crud.js
@@ -1,19 +1,19 @@
 import { db } from '$lib/db';
 
 // Simpan satu item ke DB
-async function saveToIndexedDB(entry) {
+export async function SaveToIndexedDB(entry) {
   await db.vault.add(entry);
   await loadFromIndexedDB();
 }
 
 // Load semua item dari DB
-async function loadFromIndexedDB() {
+export async function LoadFromIndexedDB() {
   const result = await db.vault.toArray();
   data = result;
 }
 
 // Hapus item dari DB (kalau kamu mau fitur delete)
-async function deleteFromIndexedDB(id) {
+export async function DeleteFromIndexedDB(id) {
   await db.vault.delete(id);
   await loadFromIndexedDB();
 }
