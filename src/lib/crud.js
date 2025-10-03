d23e13de7a7b76a7ccc7d41e583ef5fdd009821f
naim787 <naimmmmab@gmail.com>
2025-10-03 06:51:46 -0400

    Update: 10/3/2025, 6:51:46 AM

--- src/lib/crud.js
+++ src/lib/crud.js
@@ -1,22 +1,18 @@
 import { db } from '$lib/db';
-
-import { db } from '$lib/db';
+import { writable } from 'svelte/store';
 
 export const VaultData = writable([]);
 
-// Simpan satu item ke DB
 export async function SaveToIndexedDB(entry) {
   await db.vault.add(entry);
   await LoadFromIndexedDB();
 }
 
-// Load semua item dari DB
 export async function LoadFromIndexedDB() {
   const result = await db.vault.toArray();
-   VaultData.set(result);
+  VaultData.set(result);
 }
 
-// Hapus item dari DB (kalau kamu mau fitur delete)
 export async function DeleteFromIndexedDB(id) {
   await db.vault.delete(id);
   await LoadFromIndexedDB();
