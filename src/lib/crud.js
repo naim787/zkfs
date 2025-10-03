7a5eb7bb75874a5cf1ea55fd131c2140d7ea16dc
naim787 <naimmmmab@gmail.com>
2025-10-03 08:33:16 -0400

    Update: 10/3/2025, 8:33:16 AM

--- src/lib/crud.js
+++ src/lib/crud.js
@@ -2,6 +2,12 @@ import { db } from '$lib/db';
 import { VaultData } from 'svelte/store';
 import { decryptEntry } from '$lib/crypto-field.js';
 
+// ms pasword 
+import { masterPassword } from '$lib/stores';
+
+let mp;
+masterPassword.subscribe(val => mp = val);
+
 export const VaultData = writable([]);
 
 export async function SaveToIndexedDB(entry) {
@@ -13,7 +19,7 @@ export async function LoadFromIndexedDB() {
   const raw = await db.vault.toArray();
 
   const decryptedData = await Promise.all(
-    raw.map(entry => decryptEntry(entry, masterPassword))
+    raw.map(entry => decryptEntry(entry, mp))
   );
 
   VaultData.set(decryptedData);
